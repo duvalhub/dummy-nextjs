8ca30783c2b1179649f38b40060d1f0c64fa0217
Hugues McNeil-Duval <hugues.mcneil-duval@duvalhub.com>
2023-02-10 21:08:07 -0500

    support for i18n in nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { appWithTranslation } from 'next-i18next';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+
+// i18n
+//   .use(initReactI18next) // passes i18n down to react-i18next
+//   .init({
+//     // the translations
+//     // (tip move them in a JSON file and import them,
+//     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+//     resources: {
+//       en: {
+//         translation: {
+//           "hello": "Sup dawg ! this is in english"
+//         }
+//       },
+//       fr: {
+//         translation: {
+//           "hello": "Oy Mate ! T'en veux tu du french!"
+//         }
+//       }
+//     },
+//     lng: "en", // if you're using a language detector, do not define the lng option
+//     fallbackLng: "en",
+
+//     interpolation: {
+//       escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+//     }
+//   });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default appWithTranslation(MyApp)
